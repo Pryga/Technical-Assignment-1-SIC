a0c8d604025da6c80dc648f0067ec4b779eefa67
Pryga <prayogasetiawan030203@gmail.com>
2024-06-14 23:35:07 +0700

    Update mq-2.ino

--- Code/mq-2.ino
+++ Code/mq-2.ino
@@ -1,9 +1,9 @@
 #include "WiFi.h"
 #include "HTTPClient.h"
 
-const char* ssid = "KRATON 1";        
-const char* password = "Katasandi123"; 
-const char* serverName = "http://192.168.1.12:5000/data"; //alamat IP server lokal
+const char ssid = "KRATON 1";        
+const char password = "Katasandi123"; 
+const char serverName = "http://192.168.1.12:5000/data"; //alamat IP server lokal
 const int mqPin = 4;
 
 void setup() {
